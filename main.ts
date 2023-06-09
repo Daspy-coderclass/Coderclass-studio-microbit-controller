0e7979abd6572c99c3b1244021a7dc2e9ffe48a2
Daspy-coderclass <77778303+Daspy-coderclass@users.noreply.github.com>
2023-06-09 11:04:46 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedString(function (receivedString) {
+    let waarde = ""
+    if (receivedString.length == 12) {
+        uur = waarde.substr(7, 2)
+    } else {
+        uur = waarde.substr(7, 1)
+    }
+    draaien = true
+})
+let keuze = 0
+let draaien = false
+let uur = ""
+radio.setGroup(113)
+basic.forever(function () {
+    basic.clearScreen()
+    basic.showString(uur)
+    if (draaien) {
+        if (parseFloat(uur) == 21) {
+            keuze = 60
+        } else if (parseFloat(uur) == 7) {
+            keuze = 0
+        }
+    }
+})
